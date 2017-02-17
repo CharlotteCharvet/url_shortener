b41b1ade42e240b484828b44080c3e63a47b9b5a
CharlotteCharvet <charlotte.lannon@gmail.com>
2017-02-17 16:35:01 +0800

    validation in the url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,5 @@
 class Url < ApplicationRecord
+	validates :longUrl, uniqueness: true
+	validates :longUrl, presence:{ message:"nothing inside sorry, i need an url!"}
+	validates_format_of :longUrl, :with => URI::regexp(%w(http https)), :message => "Enter a valid url, please!"
 end
